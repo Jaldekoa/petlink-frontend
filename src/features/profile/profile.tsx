68ce3e6523b9da08832af6aad00222c350f42fbe
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-25 12:51:31 +0200

    Add profile and ProfileHeader components

---
+++ src/features/profile/profile.tsx
@@ -0,0 +1,13 @@
+import ProfileHeader from "./ProfileHeader";
+
+export default function Profile() {
+  return (
+    <>
+      <ProfileHeader
+        profileImg="https://lh3.googleusercontent.com/aida-public/AB6AXuAQ-JSE5wW462cJzhtZwDHn6afzzyrE3IxiFspqBlijsDYFwFkI1f1wVP_3pqJpXxTVi3P0eTBsuzw7J02g238pQ1yFyCK1r6fTClg--vw7TKiqIHJ2uqgdZ_ugA0W48U5u-fr4yxuMZuUwn_zYIy57A7hGcR1B4OfTtulIi5OugDQd1rU6msIchcCpF5N9kL3sUTQERfvRjCQROZeR_h8VjBREF99dHQYkibyRuHt4IhEZXhthm9uPbgraHlV4xvWIIFbA3LhL2vJw"
+        profileName="María García"
+        profileLocation="Madrid, España"
+      />
+    </>
+  );
+}
