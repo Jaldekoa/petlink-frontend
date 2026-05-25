4870b5cc1aed3e5478bf0b05edd62dd2a3fe671f
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-25 22:30:04 +0200

    Remove Main and add main components to home

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,14 +1,14 @@
-import TopBar from "@/shared/components/main/TopBar";
-import NavBar from "@/shared/components/main/NavBar";
-import Main from "@/shared/components/main/Main"
+import Hero from "@shared/components/main/Hero";
+import Stats from "@shared/components/main/Stats";
+import Adoptions from "@shared/components/main/Adoptions";
 
 function Home() {
   return (
-    <>
-      <TopBar imgSrc="https://lh3.googleusercontent.com/aida-public/AB6AXuBtAc0QDa8H-BzEM8ijk6L84ypQcDmyEI7aIWu9Pd6-ja2wim1b4cvFyey4SAXkEq-R2LbyyH0SPktJhIC7z7gl38uLIub0SqH9187Awes-DirqiAuk5WsxjqTHE4Kb5c9M8vD6bs6AJJGGKyI6A-5CoCd-ShZ_XULfaz8CnEkyXv2M0321VsU1gdTl_-qFmu6zOJCj9-WIXOheekPaJX_lKRmxwGGeIS92k1onfJmnMr1Twfgvf5bvNStX_w8zsd60z89_bWNbxkN0" />
-      <Main />
-      <NavBar />
-    </>
+    <main className="mt-20 px-margin-mobile max-w-7xl mx-auto space-y-stack-lg">
+      <Hero />
+      <Stats />
+      <Adoptions />
+    </main>
   );
 }
 
