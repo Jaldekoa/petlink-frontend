b437b407f41148dc7218f3026ff916e43267d02e
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-25 22:29:24 +0200

    Create Layout with TopBar and NavBar

---
+++ src/shared/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router'
+import TopBar from "@shared/components/main/TopBar";
+import NavBar from "@shared/components/main/NavBar";
+
+export default function MainLayout() {
+    return (
+        <>
+            <TopBar imgSrc="https://lh3.googleusercontent.com/aida-public/AB6AXuBtAc0QDa8H-BzEM8ijk6L84ypQcDmyEI7aIWu9Pd6-ja2wim1b4cvFyey4SAXkEq-R2LbyyH0SPktJhIC7z7gl38uLIub0SqH9187Awes-DirqiAuk5WsxjqTHE4Kb5c9M8vD6bs6AJJGGKyI6A-5CoCd-ShZ_XULfaz8CnEkyXv2M0321VsU1gdTl_-qFmu6zOJCj9-WIXOheekPaJX_lKRmxwGGeIS92k1onfJmnMr1Twfgvf5bvNStX_w8zsd60z89_bWNbxkN0" />
+            <Outlet />
+            <NavBar />
+        </>
+    )
+}
