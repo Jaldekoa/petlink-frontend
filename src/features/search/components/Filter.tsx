7528330daeeeee9f67fb31d37db1b79f76b1c820
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-26 01:20:13 +0200

    Add Filter component

---
+++ src/features/search/components/Filter.tsx
@@ -0,0 +1,8 @@
+export default function Filter({ title, icon }: { title: string, icon: string }) {
+    return (
+        <button
+            className="flex items-center gap-2 px-6 py-2.5 bg-primary text-on-primary rounded-full font-label-md whitespace-nowrap shadow-md active:scale-95 transition-transform">
+            <span className="material-symbols-outlined text-[20px]">{icon}</span> {title}
+        </button>
+    )
+}
