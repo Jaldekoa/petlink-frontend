1d0195d0f078f0ddf5709b17738ed54e315ee6ae
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-25 17:05:41 +0200

    Add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@routes": path.resolve(__dirname, "./src/routes/"),
+      "@components": path.resolve(__dirname, "./src/shared/components/"),
+      "@hooks": path.resolve(__dirname, "./src/shared/hooks/"),
+      "@features": path.resolve(__dirname, "./src/features/"),
+      "@ui": path.resolve(__dirname, "./src/shared/ui/"),
+      "@shared": path.resolve(__dirname, "./src/shared/"),
+      "@services": path.resolve(__dirname, "./src/services/"),
     },
   },
 });
