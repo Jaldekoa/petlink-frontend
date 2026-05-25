2dcee223fd1b7aa137996f9ceac71b6e6ed9967f
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-25 02:16:13 +0200

    Add Main component

---
+++ src/shared/components/main/Main.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/shared/components/main/Hero";
+
+export default function Main() {
+    return (
+        <main className="mt-20 px-margin-mobile max-w-7xl mx-auto space-y-stack-lg">
+            <Hero />
+        </main>
+    )
+}
