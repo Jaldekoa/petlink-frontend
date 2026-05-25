e3d9c87905a9117d8d0cdcbce2d72e215a873118
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-25 23:43:32 +0200

    Add components to profile page

--- src/features/profile/profile.tsx
+++ src/features/profile/profile.tsx
@@ -1,13 +1,17 @@
-import ProfileHeader from "./ProfileHeader";
+import MenuNavigationGrid from "./components/MenuNavigationgrid";
+import ProfileHeader from "./components/ProfileHeader";
 
 export default function Profile() {
   return (
     <>
-      <ProfileHeader
-        profileImg="https://lh3.googleusercontent.com/aida-public/AB6AXuAQ-JSE5wW462cJzhtZwDHn6afzzyrE3IxiFspqBlijsDYFwFkI1f1wVP_3pqJpXxTVi3P0eTBsuzw7J02g238pQ1yFyCK1r6fTClg--vw7TKiqIHJ2uqgdZ_ugA0W48U5u-fr4yxuMZuUwn_zYIy57A7hGcR1B4OfTtulIi5OugDQd1rU6msIchcCpF5N9kL3sUTQERfvRjCQROZeR_h8VjBREF99dHQYkibyRuHt4IhEZXhthm9uPbgraHlV4xvWIIFbA3LhL2vJw"
-        profileName="María García"
-        profileLocation="Madrid, España"
-      />
+      <main className="flex-grow pt-24 pb-32 px-margin-mobile max-w-7xl mx-auto w-full">
+        <ProfileHeader
+          profileImg="https://lh3.googleusercontent.com/aida-public/AB6AXuAQ-JSE5wW462cJzhtZwDHn6afzzyrE3IxiFspqBlijsDYFwFkI1f1wVP_3pqJpXxTVi3P0eTBsuzw7J02g238pQ1yFyCK1r6fTClg--vw7TKiqIHJ2uqgdZ_ugA0W48U5u-fr4yxuMZuUwn_zYIy57A7hGcR1B4OfTtulIi5OugDQd1rU6msIchcCpF5N9kL3sUTQERfvRjCQROZeR_h8VjBREF99dHQYkibyRuHt4IhEZXhthm9uPbgraHlV4xvWIIFbA3LhL2vJw"
+          profileName="María García"
+          profileLocation="Madrid, España"
+        />
+        <MenuNavigationGrid />
+      </main>
     </>
   );
 }
