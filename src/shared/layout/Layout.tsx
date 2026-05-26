b933d9a6589c03d5f52d3ccd80f07e8245010df7
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-26 22:01:49 +0200

    Add new Header component

--- src/shared/layout/Layout.tsx
+++ src/shared/layout/Layout.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router'
-import TopBar from "@shared/components/main/TopBar";
 import NavBar from "@shared/components/main/NavBar";
+import Header from '@shared/components/main/Header';
+import { Outlet } from 'react-router'
 
 export default function MainLayout() {
     return (
         <>
-            <TopBar imgSrc="https://lh3.googleusercontent.com/aida-public/AB6AXuBtAc0QDa8H-BzEM8ijk6L84ypQcDmyEI7aIWu9Pd6-ja2wim1b4cvFyey4SAXkEq-R2LbyyH0SPktJhIC7z7gl38uLIub0SqH9187Awes-DirqiAuk5WsxjqTHE4Kb5c9M8vD6bs6AJJGGKyI6A-5CoCd-ShZ_XULfaz8CnEkyXv2M0321VsU1gdTl_-qFmu6zOJCj9-WIXOheekPaJX_lKRmxwGGeIS92k1onfJmnMr1Twfgvf5bvNStX_w8zsd60z89_bWNbxkN0" />
+            <Header />
             <Outlet />
             <NavBar />
         </>
