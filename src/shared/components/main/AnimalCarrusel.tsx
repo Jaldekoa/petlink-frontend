c9613ed0b0dc6abbf5f5ca7f240a81052bc007f4
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-26 21:06:11 +0200

    Prettier

--- src/shared/components/main/AnimalCarrusel.tsx
+++ src/shared/components/main/AnimalCarrusel.tsx
@@ -1,16 +1,32 @@
-export default function AnimalCarrusel({title, viewAllHref, children}: {title: string, viewAllHref: string, children: React.ElementType}){
-<section className="space-y-stack-md">
-            <div className="flex justify-between items-end">
-                <h3 className="font-headline-md text-headline-md text-primary">{title</h3>
-                <a className="font-label-md text-secondary hover:underline" href={viewAllHref}>Ver todos</a>
-            </div>
-            <div className="flex gap-gutter-mobile overflow-x-auto no-scrollbar pb-8 -mx-margin-mobile px-margin-mobile snap-x">
-                {children}
-            </div>
-        </section>
+import type { ReactNode } from "react";
+
+interface AnimalCarruselProps {
+  title: string;
+  viewAllHref: string;
+  children: ReactNode;
 }
 
-{/* 
-<AnimalCarrusel> titl="">
-    <Animal/>
-</AnimalCarrusel> */}
+export default function AnimalCarrusel({
+  title,
+  viewAllHref,
+  children,
+}: AnimalCarruselProps) {
+  return (
+    <section className="space-y-stack-md">
+      <div className="flex justify-between items-end">
+        <h3 className="font-headline-md text-headline-md text-primary">
+          {title}
+        </h3>
+        <a
+          className="font-label-md text-secondary hover:underline"
+          href={viewAllHref}
+        >
+          Ver todos
+        </a>
+      </div>
+      <div className="flex gap-gutter-mobile overflow-x-auto no-scrollbar pb-8 -mx-margin-mobile px-margin-mobile snap-x">
+        {children}
+      </div>
+    </section>
+  );
+}
