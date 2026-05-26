24ce97863605bc5a132d00048819b45f8d135f42
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-27 01:41:09 +0200

    Change Seearch component

--- src/features/search/search.tsx
+++ src/features/search/search.tsx
@@ -1,5 +1,6 @@
 import SearchBar from "./components/SearchBar";
 import SearchFilters from "./components/SearchFilters";
+import SearchHistory from "./components/SearchHistory";
 
 export default function Search() {
   return (
@@ -7,6 +8,7 @@ export default function Search() {
       <main className="mt-20 px-margin-mobile max-w-7xl mx-auto space-y-stack-lg">
         <SearchBar />
         <SearchFilters />
+        <SearchHistory historyItems={["Labrador Retriever", "Gato British Shorthair"]} />
       </main>
     </>
   );
