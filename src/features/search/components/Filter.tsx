256e592378e3ac573ed7425d42ae9e3feb4f4232
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-26 23:35:56 +0200

    Add interface to Filter props

--- src/features/search/components/Filter.tsx
+++ src/features/search/components/Filter.tsx
@@ -1,4 +1,9 @@
-export default function Filter({ title, icon }: { title: string, icon: string }) {
+interface FilterProps {
+    title: string;
+    icon?: string;
+}
+
+export default function Filter({ title, icon }: FilterProps) {
     return (
         <button
             className="flex items-center gap-2 px-6 py-2.5 bg-primary text-on-primary rounded-full font-label-md whitespace-nowrap shadow-md active:scale-95 transition-transform">
