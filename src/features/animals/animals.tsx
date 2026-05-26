df10943e7f9397bde2a059015f809a990544eb9f
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-27 01:49:24 +0200

    Fix the fix

---
+++ src/features/animals/animals.tsx
@@ -0,0 +1,13 @@
+import Categories from "./components/Categories";
+import Headline from "./components/Headline";
+
+
+export default function Animals() {
+    return (
+        <main className="mt-20 px-margin-mobile max-w-7xl mx-auto space-y-stack-lg">
+            <Headline />
+            <Categories />
+
+        </main>
+    )
+}
