3e925aa6622d229fc495555b26811c60e6bccedb
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-27 09:50:07 +0200

    Change Link component with NavLink

--- src/shared/components/main/NavBar.tsx
+++ src/shared/components/main/NavBar.tsx
@@ -1,41 +1,49 @@
-import { Link } from 'react-router';
+import { NavLink } from "react-router";
 
 export default function NavBar() {
   return (
-    <nav
-      className="fixed bottom-0 left-0 w-full z-50 bg-surface-container-lowest shadow-[0_-4px_20px_0px_rgba(0,0,0,0.05)] rounded-t-xl">
+    <nav className="fixed bottom-0 left-0 w-full z-50 bg-surface-container-lowest shadow-[0_-4px_20px_0px_rgba(0,0,0,0.05)] rounded-t-xl">
       <div className="flex justify-around items-center w-full px-2 pt-2 pb-safe-bottom min-h-18">
-
-        <Link to="/"
-          className="cursor-pointer flex flex-col items-center justify-center bg-secondary-container text-on-secondary-container rounded-full px-5 py-1.5 transition-all duration-300">
+        <NavLink
+          to="/"
+          className="cursor-pointer flex flex-col items-center justify-center bg-secondary-container text-on-secondary-container rounded-full px-5 py-1.5 transition-all duration-300"
+        >
           <span className="material-symbols-outlined">home</span>
           <span className="font-label-sm">Inicio</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/animals"
-          className="flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all">
+        <NavLink
+          to="/animals"
+          className="flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all"
+        >
           <span className="material-symbols-outlined">pets</span>
           <span className="font-label-sm">Mascotas</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/search"
-          className="cursor-pointer flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all">
+        <NavLink
+          to="/search"
+          className="cursor-pointer flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all"
+        >
           <span className="material-symbols-outlined">search</span>
           <span className="font-label-sm">Buscar</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/chat"
-          className="flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all">
+        <NavLink
+          to="/chat"
+          className="flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all"
+        >
           <span className="material-symbols-outlined">chat</span>
           <span className="font-label-sm">Chat</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/profile"
-          className="cursor-pointer flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all">
+        <NavLink
+          to="/profile"
+          className="cursor-pointer flex flex-col items-center justify-center text-on-surface-variant px-5 py-1.5 hover:bg-surface-container-high rounded-full active:scale-95 transition-all"
+        >
           <span className="material-symbols-outlined">person</span>
           <span className="font-label-sm">Perfil</span>
-        </Link>
+        </NavLink>
       </div>
     </nav>
-  )
+  );
 }
